d59cf76bee63c7e681d75864f34a2fc3cd81fd0e
Guilherme Santana <gf.santana@campus.fct.unl.pt>
2023-06-15 01:57:00 +0100

    #20 conteúdo do email em string
    TODO: Tratar string e enviar para o frontEnd

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/Gmailer.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/Gmailer.java
@@ -7,30 +7,22 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonError;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Draft;
-import com.google.api.services.gmail.model.ListMessagesResponse;
-import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.*;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
 
 public class Gmailer {
 
@@ -49,6 +41,9 @@ public class Gmailer {
             "}";
 
 
+    /**
+     * Email of the Service Account. Can use "me" instead.
+     */
     private static final String EMAIL = "devs.in.touch@gmail.com";
 
     /**
@@ -60,9 +55,6 @@ public class Gmailer {
      * Global instance of the JSON factory.
      */
     private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-
-    private static final String CREDENTIALS_FILE_PATH = "/client_secret_635132466395-ahq2fu08h9u7l3r21joiuoffm2eph0an" +
-            ".apps.googleusercontent.com.json";
 
     private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_MODIFY);
     public static final String TOKENS_PATH = "tokens";
@@ -102,7 +94,12 @@ public class Gmailer {
     }
 
 
-    private void listSocialEmails(){
+    /**
+     * List all Messages of the user's mailbox in the social label.
+     * This label was the FCT diary email with the info required.
+     * @return the list of messages if successful.
+     */
+    private List<Message> listSocialEmails(){
         try{
             //List all emails from social media
             String query = "category:social";
@@ -118,22 +115,53 @@ public class Gmailer {
                     break;
                 }
             }
-            for (Message message : messages) {
+            /*for (Message message : messages) {
                 service.users().messages().trash("me", message.getId()).execute();
-            }
+            }*/
+            return messages;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void print(){
+        List<Message> messages =  listSocialEmails();
+        System.out.println("Total messages: " + messages.size());
+        for(Message message : messages){
+            getMessageContent(message);
         }
 
     }
 
-    private void sendMail(String subject, String body) throws GeneralSecurityException, IOException, MessagingException {
+    private void getMessageContent(Message message) {
+        try {
+            Message fullMessage = service.users().messages().get("me", message.getId()).setFormat("full").execute();
+            List<MessagePart> parts = fullMessage.getPayload().getParts();
+            List<MessagePart> insideParts = parts.get(0).getParts();
+
+            if (insideParts != null && !insideParts.isEmpty()) {
+                for (MessagePart part : insideParts) {
+                    String mimeType = part.getMimeType();
+                    if(mimeType.startsWith("text/plain")){
+                        MessagePartBody bodyParts = part.getBody();
+                        String data = bodyParts.getData();
+                        byte[] decodedBytes = Base64.decodeBase64(data);
+                        String decodedData = new String(decodedBytes, StandardCharsets.UTF_8);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+   /* private void sendMail(String subject, String body) throws GeneralSecurityException, IOException, MessagingException {
         //send email to yourself
 
 
-      /*  // Create the email content
+      *//*  // Create the email content
         String messageSubject = "Test message";
-        String bodyText = "lorem ipsum.";*/
+        String bodyText = "lorem ipsum.";*//*
 
         // Encode as MIME message
         Properties props = new Properties();
@@ -145,8 +173,8 @@ public class Gmailer {
         email.setSubject(subject);
         email.setText(body);
 
-       /* MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(bodyText, "text/plain");
+       *//* MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(body, "text/plain");
         Multipart multipart = new MimeMultipart();
         multipart.addBodyPart(mimeBodyPart);
         mimeBodyPart = new MimeBodyPart();
@@ -154,13 +182,13 @@ public class Gmailer {
         mimeBodyPart.setDataHandler(new DataHandler(source));
         mimeBodyPart.setFileName(file.getName());
         multipart.addBodyPart(mimeBodyPart);
-        email.setContent(multipart);*/
+        email.setContent(multipart);*//*
 
         // Encode and wrap the MIME message into a gmail message
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         email.writeTo(buffer);
         byte[] rawMessageBytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        String encodedEmail = encodeBase64URLSafeString(rawMessageBytes);
         Message message = new Message();
         message.setRaw(encodedEmail);
 
@@ -177,35 +205,11 @@ public class Gmailer {
                 throw e;
             }
         }
-    }
+    }*/
 
     public static void main(String[] args) throws GeneralSecurityException, IOException, MessagingException {
-
-        File file = new File(CREDENTIALS_FILE_PATH);
-        System.out.println("File name: " + file.getName());
-        System.out.println("Absolute path: " + file.getAbsolutePath());
-        // Obter o diretório atual
-        /*String diretorioAtual = "src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/";
-        System.out.println("Diretório atual: " + diretorioAtual);
-
-        // Criação do objeto File com o diretório atual
-        File folder = new File(diretorioAtual);
-
-        // Verifica se é um diretório válido
-        if (folder.isDirectory()) {
-            // Lista dos arquivos no diretório
-            File[] files = folder.listFiles();
-
-            // Imprime o nome dos arquivos
-            if (files != null) {
-                for (File file : files) {
-                    System.out.println(file.getName());
-                }
-            }
-        } else {
-            System.out.println("O diretório atual não é válido.");
-        }*/
-        new Gmailer().sendMail("A new message", "Esta a funcionar Chavalo");
+        //new Gmailer().sendMail("A new message", "Esta a funcionar Chavalo");
+        new Gmailer().print();
 
 
     }
