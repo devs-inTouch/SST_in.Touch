0783dfd14f94dab15ae965513c7c0112ec632cf3
Guilherme Santana <gf.santana@campus.fct.unl.pt>
2023-06-17 18:36:12 +0100

    #20 gmailer endpoint progres

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/Driver.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/Driver.java
@@ -1,52 +1,49 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
-
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.*;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.MessagePart;
+import com.google.api.services.gmail.model.MessagePartBody;
 import org.apache.commons.codec.binary.Base64;
 
-import javax.mail.MessagingException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-public class Gmailer {
-
+public class Driver {
     private static final String CLIENT_SECRET_JSON = "{\n" +
             "  \"installed\": {\n" +
-            "    \"client_id\": \"635132466395-ahq2fu08h9u7l3r21joiuoffm2eph0an.apps.googleusercontent.com\",\n" +
+            "    \"client_id\": \"635132466395-ne89k6bsob8nclc53gk7334gomeqbvo1.apps.googleusercontent.com\",\n" +
             "    \"project_id\": \"steel-sequencer-385510\",\n" +
             "    \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n" +
             "    \"token_uri\": \"https://oauth2.googleapis.com/token\",\n" +
             "    \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n" +
-            "    \"client_secret\": \"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\",\n" +
+            "    \"client_secret\": \"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\",\n" +
             "    \"redirect_uris\": [\n" +
             "      \"http://localhost\"\n" +
             "    ]\n" +
             "  }\n" +
             "}";
-
-
-    /**
-     * Email of the Service Account. Can use "me" instead.
-     */
-    private static final String EMAIL = "devs.in.touch@gmail.com";
 
     /**
      * Application name.
@@ -58,20 +55,33 @@ public class Gmailer {
      */
     private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
+    public static final String TOKENS_PATH = "tokensDrive";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
     private static final List<String> SCOPES = Arrays.asList(
+            GmailScopes.GMAIL_MODIFY,
             DriveScopes.DRIVE,
-            GmailScopes.GMAIL_MODIFY
+            DriveScopes.DRIVE_APPDATA,
+            DriveScopes.DRIVE_FILE
     );
-    public static final String TOKENS_PATH = "tokens";
 
-    private final Gmail service;
+    private final Drive serviceDrive;
+    private final Gmail serviceGmail;
 
-    public Gmailer() throws IOException, GeneralSecurityException {
+
+    public Driver() throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        serviceDrive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+        serviceGmail = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
     }
 
     /**
@@ -89,32 +99,69 @@ public class Gmailer {
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(Paths.get(TOKENS_PATH).toFile()))
+                .setDataStoreFactory(AppEngineDataStoreFactory.getDefaultInstance())
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
         //returns an authorized Credential object.
         return credential;
     }
 
+    private void listFiles() throws IOException {
+// Print the names and IDs for up to 10 files.
+        FileList result = serviceDrive.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.isEmpty()) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+    }
 
-    /**
-     * List all Messages of the user's mailbox in the social label.
-     * This label was the FCT diary email with the info required.
-     * @return the list of messages if successful.
-     */
+    private void uploadFile(java.io.File file) throws IOException {
+        File driveFile = new File();
+        driveFile.setName(file.getName());
+
+        FileContent mediaContent = new FileContent(null, file);
+
+        File uploadedFile = serviceDrive.files().create(driveFile, mediaContent)
+                .setFields("id")
+                .execute();
+
+        System.out.println("File uploaded. File ID: " + uploadedFile.getId());
+    }
+
+    private void createDirectory(String directoryName) throws IOException {
+        File driveDirectory = new File();
+        driveDirectory.setName(directoryName);
+        driveDirectory.setMimeType("application/vnd.google-apps.folder");
+
+        File createdDirectory = serviceDrive.files().create(driveDirectory)
+                .setFields("id")
+                .execute();
+
+        System.out.println("Directory created. Directory ID: " + createdDirectory.getId());
+    }
+
+
     private List<Message> listSocialEmails(){
         try{
             //List all emails from social media
             String query = "category:social";
-            ListMessagesResponse response = service.users().messages().list("me").setQ(query).execute();
+            ListMessagesResponse response = serviceGmail.users().messages().list("me").setQ(query).execute();
             List<Message> messages = new ArrayList<>();
             while (response.getMessages() != null) {
                 messages.addAll(response.getMessages());
                 if (response.getNextPageToken() != null) {
                     String pageToken = response.getNextPageToken();
-                    response = service.users().messages().list("me").setQ(query)
+                    response = serviceGmail.users().messages().list("me").setQ(query)
                             .setPageToken(pageToken).execute();
                 } else {
                     break;
@@ -129,18 +176,9 @@ public class Gmailer {
         }
     }
 
-    public void print(){
-        List<Message> messages =  listSocialEmails();
-        System.out.println("Total messages: " + messages.size());
-        for(Message message : messages){
-            getMessageContent(message);
-        }
-
-    }
-
     private void getMessageContent(Message message) {
         try {
-            Message fullMessage = service.users().messages().get("me", message.getId()).setFormat("full").execute();
+            Message fullMessage = serviceGmail.users().messages().get("me", message.getId()).setFormat("full").execute();
             List<MessagePart> parts = fullMessage.getPayload().getParts();
             List<MessagePart> insideParts = parts.get(0).getParts();
 
@@ -161,65 +199,30 @@ public class Gmailer {
         }
     }
 
-   /* private void sendMail(String subject, String body) throws GeneralSecurityException, IOException, MessagingException {
-        //send email to yourself
-
-
-      *//*  // Create the email content
-        String messageSubject = "Test message";
-        String bodyText = "lorem ipsum.";*//*
-
-        // Encode as MIME message
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-        MimeMessage email = new MimeMessage(session);
-        email.setFrom(new InternetAddress(EMAIL));
-        email.addRecipient(javax.mail.Message.RecipientType.TO,
-                new InternetAddress(EMAIL));
-        email.setSubject(subject);
-        email.setText(body);
-
-       *//* MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(body, "text/plain");
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
-        mimeBodyPart = new MimeBodyPart();
-        DataSource source = new FileDataSource(file);
-        mimeBodyPart.setDataHandler(new DataHandler(source));
-        mimeBodyPart.setFileName(file.getName());
-        multipart.addBodyPart(mimeBodyPart);
-        email.setContent(multipart);*//*
-
-        // Encode and wrap the MIME message into a gmail message
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        email.writeTo(buffer);
-        byte[] rawMessageBytes = buffer.toByteArray();
-        String encodedEmail = encodeBase64URLSafeString(rawMessageBytes);
-        Message message = new Message();
-        message.setRaw(encodedEmail);
-
-        try {
-            message = service.users().messages().send("me", message).execute();
-            System.out.println("Draft id: " + message.getId());
-            System.out.println(message.toPrettyString());
-        } catch (GoogleJsonResponseException e) {
-            // TODO(developer) - handle error appropriately
-            GoogleJsonError error = e.getDetails();
-            if (error.getCode() == 403) {
-                System.err.println("Unable to create draft: " + e.getDetails());
-            } else {
-                throw e;
-            }
+    public void print(){
+        List<Message> messages =  listSocialEmails();
+        System.out.println("Total messages: " + messages.size());
+        for(Message message : messages){
+            getMessageContent(message);
         }
-    }*/
-
-    public static void main(String[] args) throws GeneralSecurityException, IOException, MessagingException {
-        //new Gmailer().sendMail("A new message", "Esta a funcionar Chavalo");
-        new Gmailer().print();
-
 
     }
 
 
 
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        Driver driver = new Driver();
+        driver.listFiles();
+        driver.createDirectory("test");
+
+     /*   // Provide the file path to upload
+        java.io.File fileToUpload = new java.io.File("logo.png");
+        // Call the uploadFile method to upload the file
+        driver.uploadFile(fileToUpload);*/
+
+
+        driver.print();
+
+    }
 }
