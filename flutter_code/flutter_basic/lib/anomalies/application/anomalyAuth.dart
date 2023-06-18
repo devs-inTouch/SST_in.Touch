60ec02b0de3f05a53039cc27e795d1dcbeed56a4
Guilherme Santana <gf.santana@campus.fct.unl.pt>
2023-06-18 21:56:44 +0100

    #36 AnomaliyAuth e AnomalyPresention done

--- flutter_code/flutter_basic/lib/anomalies/application/anomalyAuth.dart
+++ flutter_code/flutter_basic/lib/anomalies/application/anomalyAuth.dart
@@ -1,33 +1,31 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class AnomalyPost{
-
-  static Future<bool> anomalyCreation(String username,String title,String description) async {
-    print("aqui");
+class AnomalyAuth {
+  static Future<bool> anomalyCreation(
+      String username, String title, String description) async {
+    print("aquiAnolmaly");
     bool res = await fetchAuthenticate(username, title, description);
 
     return res;
-
   }
 
-
-  static Future<bool> fetchAuthenticate(String username, String title, String description) async {
-
+  static Future<bool> fetchAuthenticate(
+      String username, String title, String description) async {
     final response = await http.post(
-      Uri.parse('https://steel-sequencer-385510.oa.r.appspot.com/rest/anomaly/create'),
+      Uri.parse(
+          'https://steel-sequencer-385510.oa.r.appspot.com/rest/anomaly/create'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
       body: jsonEncode(<String, String>{
-        "username":username,
-        "title":title,
-        "description":description,
-
+        "username": username,
+        "title": title,
+        "description": description,
       }),
     );
     print(response.statusCode);
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
       print("RESPOSTA: $jsonResponse");
 
@@ -36,5 +34,4 @@ class AnomalyPost{
       return false;
     }
   }
-
 }
--- flutter_code/flutter_basic/lib/anomalies/presentation/anomaliesPage.dart
+++ flutter_code/flutter_basic/lib/anomalies/presentation/anomaliesPage.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/anomalies/application/anomalyAuth.dart';
 import 'package:uuid/uuid.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,42 +10,72 @@ import 'package:image/image.dart' as Im;
 
 import '../../constants.dart';
 import '../../feeds/anomalyBox.dart';
+import '../../mainpage/presentation/desktop_main_scaffold.dart';
+import '../../mainpage/presentation/mobile_main_scaffold.dart';
+import '../../mainpage/presentation/responsive_main_page.dart';
+import '../../mainpage/presentation/tablet_main_scaffold.dart';
 import '../../myAppBar.dart';
 
 class AnomaliesPage extends StatefulWidget {
-
-    State<AnomaliesPage> createState() => AnomalyState();
+  State<AnomaliesPage> createState() => AnomalyState();
 }
 
 class AnomalyState extends State<AnomaliesPage> {
-
-  List anomalyPosts = [
-    "post 1","post 2", "post 3", "post 4"
-  ];
+  List anomalyPosts = ["post 1", "post 2", "post 3", "post 4"];
 
   late File file;
   String postId = Uuid().v4();
-  final  storageRef = FirebaseStorage.instance.ref();
+  final storageRef = FirebaseStorage.instance.ref();
 
   TextEditingController description = TextEditingController();
   TextEditingController title = TextEditingController();
 
   bool isUploading = false;
 
+  void createButtonPressed(String username, String title, String description) {
+    AnomalyAuth.anomalyCreation(username, title, description).then((isCreated) {
+      if (isCreated) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const ResponsiveLayout(
+                    desktopScaffold: DesktopScaffold(),
+                    mobileScaffold: MobileScaffold(),
+                    tabletScaffold: TabletScaffold())));
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Parametros incorretos'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    });
+  }
 
   handleTakePhoto(context) async {
     Navigator.pop(context);
-    File file = (await ImagePicker().pickImage(source: ImageSource.camera,
-        maxHeight: 675, maxWidth: 960)) as File;
+    File file = (await ImagePicker().pickImage(
+        source: ImageSource.camera, maxHeight: 675, maxWidth: 960)) as File;
     setState(() {
       this.file = file;
     });
   }
 
-  handleChoosePhoto(context) async{
+  handleChoosePhoto(context) async {
     Navigator.pop(context);
-    File file = (await ImagePicker().pickImage(source: ImageSource.gallery,
-        maxHeight: 675, maxWidth: 960)) as File;
+    File file = (await ImagePicker().pickImage(
+        source: ImageSource.gallery, maxHeight: 675, maxWidth: 960)) as File;
     setState(() {
       this.file = file;
     });
@@ -58,18 +88,15 @@ class AnomalyState extends State<AnomaliesPage> {
     return downloadUrl;
   }
 
-  putInDatabase({required String mediaUrl, required String title, required String desc}) {
+  putInDatabase(
+      {required String mediaUrl, required String title, required String desc}) {
     // fazer pedido REST
   }
 
   submitPost() async {
     await compressImage();
     String url = await uploadImage(file);
-    putInDatabase(
-        mediaUrl: url,
-        title: title.text,
-        desc: description.text
-    );
+    putInDatabase(mediaUrl: url, title: title.text, desc: description.text);
     description.clear();
     setState(() {
       isUploading = false;
@@ -80,7 +107,8 @@ class AnomalyState extends State<AnomaliesPage> {
     final tempDir = await getTemporaryDirectory();
     final path = tempDir.path;
     Im.Image? image = Im.decodeImage(file.readAsBytesSync());
-    final compressedImage = File('$path/img_$postId.jpg')..writeAsBytesSync(Im.encodeJpg(image!, quality: 85));
+    final compressedImage = File('$path/img_$postId.jpg')
+      ..writeAsBytesSync(Im.encodeJpg(image!, quality: 85));
     setState(() {
       file = compressedImage;
     });
@@ -92,30 +120,30 @@ class AnomalyState extends State<AnomaliesPage> {
     });
     await compressImage();
     String mediaUrl = await uploadImage(file);
-    putInDatabase(mediaUrl: mediaUrl, title: title.text, desc: description.text);
+    putInDatabase(
+        mediaUrl: mediaUrl, title: title.text, desc: description.text);
     description.clear();
     setState(() {
       isUploading = false;
     });
   }
 
-
-
   selectImage(parentContext) {
-    return showDialog(context: parentContext,
+    return showDialog(
+        context: parentContext,
         builder: (context) {
           return SimpleDialog(
             title: Text('Imagem'),
             children: [
               SimpleDialogOption(
                 child: Text('Escolhe da galeria'),
-                onPressed: () {handleChoosePhoto(context);},
+                onPressed: () {
+                  handleChoosePhoto(context);
+                },
               ),
               SimpleDialogOption(
                   child: Text('Cancelar'),
-                  onPressed: () => Navigator.pop(context)
-
-              )
+                  onPressed: () => Navigator.pop(context))
             ],
           );
         });
@@ -133,136 +161,110 @@ class AnomalyState extends State<AnomaliesPage> {
       body: Stack(
         children: [
           Container(
-
             width: size.width,
             height: size.height,
-            child:Scrollbar(
+            child: Scrollbar(
               child: SingleChildScrollView(
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Padding(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text(
-                              'ANOMALY',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 60
-
-                              ),
-                            )),
-                        Container(
-                            height: 500*fem,
-                            width: 600*fem,
-                            decoration: BoxDecoration(
-                                color: Colors.grey[300]
-                            ),
-                            child: Stack(
-                              children: [
-                                isUploading ? LinearProgressIndicator() : Text(""),
-                                Align(
-                                  alignment: Alignment(0.0, 0.1),
-                                  child: Container(
-                                    height: 400 * fem,
-                                    width: 570 * fem,
-                                    child: Column(
-                                      children: [
-                                        TextField(
-                                          controller: title,
-                                          maxLength: 50,
-                                          decoration: InputDecoration(
-                                            fillColor: Colors.white,
-                                            filled: true,
-                                            labelText: 'Título',
-                                          ),
-                                        ),
-                                        SizedBox(height: 10),
-                                        TextField(
-                                          controller: description,
-                                          keyboardType: TextInputType.multiline,
-                                          maxLines: 5,
-                                          maxLength: 400,
-                                          decoration: InputDecoration(
-                                            fillColor: Colors.white,
-                                            filled: true,
-                                            labelText: 'Escreve aqui',
-                                          ),
-                                        ),
-                                      ],
+                    Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'ANOMALY',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.black, fontSize: 60),
+                        )),
+                    Container(
+                        height: 500 * fem,
+                        width: 600 * fem,
+                        decoration: BoxDecoration(color: Colors.grey[300]),
+                        child: Stack(
+                          children: [
+                            isUploading ? LinearProgressIndicator() : Text(""),
+                            Align(
+                              alignment: Alignment(0.0, 0.1),
+                              child: Container(
+                                height: 400 * fem,
+                                width: 570 * fem,
+                                child: Column(
+                                  children: [
+                                    TextField(
+                                      controller: title,
+                                      maxLength: 50,
+                                      decoration: InputDecoration(
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        labelText: 'Título',
+                                      ),
                                     ),
-                                  ),
+                                    SizedBox(height: 10),
+                                    TextField(
+                                      controller: description,
+                                      keyboardType: TextInputType.multiline,
+                                      maxLines: 5,
+                                      maxLength: 400,
+                                      decoration: InputDecoration(
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        labelText: 'Escreve aqui',
+                                      ),
+                                    ),
+                                  ],
                                 ),
-
-                                Align( //Botao para adicionar foto
-                                    alignment: Alignment(0.9,0.9),
-                                    child:
-                                    ElevatedButton(
-                                        onPressed: isUploading ? null : () =>  handleSubmit(),
-                                        style: ElevatedButton.styleFrom(
-                                            fixedSize: Size(70* fem, 40* fem),
-                                            backgroundColor: Colors.white
-                                        ),
-                                        child: Text(
-                                          'CRIAR',
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 12
-
-                                          ),
-
-                                        )
-
-                                    )),
-                                Align( //Botao para adicionar foto
-                                    alignment: Alignment(-0.9,0.9),
-                                    child:
-                                    ElevatedButton(
-                                        onPressed: () { selectImage(context);},
-                                        style: ElevatedButton.styleFrom(
-                                            fixedSize: Size(100* fem, 40* fem),
-                                            backgroundColor: Colors.white
-                                        ),
-                                        child: Text(
-                                          'Adicionar foto',
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 15
-
-                                          ),
-
-                                        )
-
-                                    ))
-                              ],
-                            )
-                        ),
-                        SizedBox(height: 10),
-                        Container(
-                            width: 600*fem,
-                            child:ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: anomalyPosts.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                return AnomalyBox(text:anomalyPosts[index], fem: fem);
-                              },
-
-                            )
-                        )
-                      ]
-                  )
-
-
-              ),
+                              ),
+                            ),
+                            Align(
+                                //Botao para adicionar foto
+                                alignment: Alignment(0.9, 0.9),
+                                child: ElevatedButton(
+                                    onPressed: isUploading
+                                        ? null
+                                        : () => createButtonPressed("gui",
+                                            title.text, description.text),
+                                    style: ElevatedButton.styleFrom(
+                                        fixedSize: Size(70 * fem, 40 * fem),
+                                        backgroundColor: Colors.white),
+                                    child: Text(
+                                      'CRIAR',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 12),
+                                    ))),
+                            Align(
+                                //Botao para adicionar foto
+                                alignment: Alignment(-0.9, 0.9),
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      selectImage(context);
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                        fixedSize: Size(100 * fem, 40 * fem),
+                                        backgroundColor: Colors.white),
+                                    child: Text(
+                                      'Adicionar foto',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 15),
+                                    )))
+                          ],
+                        )),
+                    SizedBox(height: 10),
+                    Container(
+                        width: 600 * fem,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: anomalyPosts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return AnomalyBox(
+                                text: anomalyPosts[index], fem: fem);
+                          },
+                        ))
+                  ])),
             ),
-
           )
         ],
       ),
     );
   }
-
-
 }
