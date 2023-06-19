99ca601b6e44d85f068dbada269672d00685ef4f
f-leao <fjaleao@gmail.com>
2023-06-19 22:53:17 +0100

    logging in - working

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/authentication/SecretManager.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/authentication/SecretManager.java
@@ -58,7 +58,8 @@ public class SecretManager {
         if (secret == null) {
             secret = init();
         }
-            signKeySpec = new SecretKeySpec(this.getSecret().getBytes(StandardCharsets.UTF_8), ALGORYTHM);
+        this.secretStr = secret.getString(VALUE);
+        signKeySpec = new SecretKeySpec(this.getSecret().getBytes(StandardCharsets.UTF_8), ALGORYTHM);
     }
 
 
