1613a62cb08fcaa2c083e58dc6431ef69e743b58
Guilherme Santana <gf.santana@campus.fct.unl.pt>
2023-07-07 11:49:49 +0100

    #51 emailJs done

--- flutter_basic/lib/register/application/registerAuth.dart
+++ flutter_basic/lib/register/application/registerAuth.dart
@@ -1,18 +1,28 @@
 import 'dart:convert';
+import 'package:emailjs/emailjs.dart';
 import 'package:http/http.dart' as http;
 
 class RegisterAuth {
+  static const String emailJsServiceID = 'service_ttj4pgn';
+  static const String emailJsTemplateID = 'template_zyyib8q';
+  static const String emailJsPublicKey = 'Rw2KylH8VjSgE6oQ-';
+  static const String emailJsPrivateKey = 'nVegrjC7Pqfkv7dV0swMd';
 
-  static bool emptyFields(String username, String email, String name, String pwd, String pwdConfirm) {
-    return username.isEmpty && email.isEmpty && name.isEmpty && pwd.isEmpty && pwdConfirm.isEmpty;
+  static bool emptyFields(String username, String email, String name,
+      String pwd, String pwdConfirm) {
+    return username.isEmpty &&
+        email.isEmpty &&
+        name.isEmpty &&
+        pwd.isEmpty &&
+        pwdConfirm.isEmpty;
   }
 
   static bool hasSpecialChars(String password) {
-    if(password.isEmpty) {
+    if (password.isEmpty) {
       return false;
     }
 
-    return  password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+    return password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
   }
 
   static bool isPasswordCompliant(String password, [int minLength = 5]) {
@@ -26,48 +36,90 @@ class RegisterAuth {
     bool hasLowercase = password.contains(RegExp(r'[a-z]'));
     bool hasMinLength = password.length > minLength;
 
-    return hasDigits &
-    hasUppercase &
-    hasLowercase &
-    hasMinLength;
+    return hasDigits & hasUppercase & hasLowercase & hasMinLength;
   }
 
-  static bool registerUser(String username, String email, String name, String pwd, String studentNumber, String course, String role,
-      String description, String department) {
-
-    fetchAuthenticate( username,  email,  name,  pwd, studentNumber, course, role,
-        description, department);
-
-
+  static bool registerUser(
+      String username,
+      String email,
+      String name,
+      String pwd,
+      String studentNumber,
+      String course,
+      String role,
+      String description,
+      String department) {
+    Map<String, dynamic> obj = {
+      "username": username,
+      "name": name,
+      "email": email,
+      "activateLink": "ADICIONAR"
+    };
+    sendEmail(obj);
     return true;
-
+    /*  fetchAuthenticate(username, email, name, pwd, studentNumber, course, role,
+            description, department)
+        .then((response) {
+      if (response) {
+        String emailBody = createEmailBody(username);
+        return true;
+      }
+    });
+    return false; */
   }
 
-  static Future<bool> fetchAuthenticate(String username, String email, String name, String pwd, String studentNumber, String course,
-      String role, String description, String department) async {
-    final response = await http.post(
-      Uri.parse('https://steel-sequencer-385510.oa.r.appspot.com/rest/register/'),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(<String, String>{
-        "username":username,
-        "name":name,
-        "email":email,
-        "password":pwd,
-        "studentNumber" : studentNumber,
-        "course": course,
-        "role":role,
-        "description":description,
-        "department":department
-      }),
-    );
-
-    if(response.statusCode == 200) {
-      return true;
-    } else {
-      return false;
+  static void sendEmail(Map<String, dynamic> obj) async {
+    try {
+      await EmailJS.send(
+        emailJsServiceID,
+        emailJsTemplateID,
+        obj,
+        const Options(
+          publicKey: emailJsPublicKey,
+          privateKey: emailJsPrivateKey,
+        ),
+      );
+      print('SUCCESS!');
+    } catch (error) {
+      if (error is EmailJSResponseStatus) {
+        print('ERROR... ${error.status}: ${error.text}');
+      }
+      print(error.toString());
     }
   }
 }
 
+Future<bool> fetchAuthenticate(
+    String username,
+    String email,
+    String name,
+    String pwd,
+    String studentNumber,
+    String course,
+    String role,
+    String description,
+    String department) async {
+  final response = await http.post(
+    Uri.parse('https://steel-sequencer-385510.oa.r.appspot.com/rest/register/'),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode(<String, String>{
+      "username": username,
+      "name": name,
+      "email": email,
+      "password": pwd,
+      "studentNumber": studentNumber,
+      "course": course,
+      "role": role,
+      "description": description,
+      "department": department
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
